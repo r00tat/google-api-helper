9745b6ac9b565340609d7b36ce55701d92f749be
Paul Woelfel <github@frig.at>
2017-05-30 15:12:49 +0200

    version 0.1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='google-api-helper',
-    version='0.1.0',
+    version='0.1.1',
 
     description='Python helper class to streamlime interaction with Google APIs. Based on python-google-api-client.',
     long_description=long_description,
